738b38b23f4549715b72e040634856b7ad0d8d59
James Richardson <jamesrichardson@James-Richardson.local>
2016-03-09 10:57:51 +0000

    README changes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dfile=src/main/resources/dockfx-0.1-SNAPSHOT.jar -DgroupId=org.dockfx -DartifactId=dockfx -Dversion=0.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=lib/dockfx-0.1-SNAPSHOT.jar -DgroupId=org.dockfx -DartifactId=dockfx -Dversion=0.1-SNAPSHOT -Dpackaging=jar
 mvn package
 java -cp target/redress-1.0-SNAPSHOT-jar-with-dependencies.jar: redress.Main
